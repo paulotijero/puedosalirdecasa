2465f42462128bc53b1869afafae297784d86bcf
paulotijero <paulotijero@gmail.com>
2020-03-25 11:43:36 -0500

    ADD: improve metas

--- pages/index.js
+++ pages/index.js
@@ -22,58 +22,32 @@ export default function Index() {
       className="mx-auto flex flex-col items-center justify-between"
     >
       <Head>
+        {/* <!-- Primary Meta Tags --> */}
         <title>¿Puedo salir de casa?</title>
         <link rel="icon" href="/question.png" />
         <meta
           name="description"
           content="¿Puedo salir de casa? La página que te resuelve todas las dudas."
         />
-        <meta name="language" content="es" />
-        <meta name="author" content="Paulo Tijero" />
-        <meta
-          name="subject"
-          content="¿Puedo salir de casa? La página que te resuelve todas las dudas."
-        />
-        <meta name="pagename" content="¿Puedo salir de casa?" />
-        <meta name="HandheldFriendly" content="True" />
-        <meta name="MobileOptimized" content="320" />
-        <meta name="robots" content="index, follow" />
-        <meta name="theme-color" content="black" />
-        <meta
-          name="apple-mobile-web-app-title"
-          content="¿Puedo salir de casa?"
-        />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta
-          href="/atom.xml"
-          type="application/atom+xml"
-          rel="alternate"
-          title="¿Puedo salir de casa?"
-        />
+
+        {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta property="og:title" content="¿Puedo salir de casa?" />
+        <meta property="og:url" content="https://puedosalirdecasa.fun/" />
         <meta
           property="og:description"
           content="¿Puedo salir de casa? La página que te resuelve todas las dudas."
         />
-        <meta property="og:image" content="/puedosalir.jpg" />
         <meta
-          property="og:image:alt"
-          content="¿Puedo salir de casa? La página que te resuelve todas las dudas."
+          property="og:image"
+          content="https://puedosalirdecasa.fun/puedosalir.jpg"
         />
-        <meta property="og:url" content="https://puedosalirdecasa.fun" />
-        <meta property="og:site_name" content="¿Puedo salir de casa?" />
-        <meta property="og:locale" content="es" />
+
+        {/* <!-- Twitter --> */}
         <meta
           name="twitter:card"
           value="summary_large_image"
           content="summary_large_image"
-        />
-        <meta name="twitter:site" value="@paulotijero" content="@paulotijero" />
-        <meta
-          name="twitter:creator"
-          value="@paulotijero"
-          content="@paulotijero"
         />
         <meta
           name="twitter:url"
@@ -94,10 +68,6 @@ export default function Index() {
           name="twitter:image"
           value="https://puedosalirdecasa.fun/puedosalir.jpg"
           content="https://puedosalirdecasa.fun/puedosalir.jpg"
-        />
-        <meta
-          name="twitter:summary"
-          content="¿Puedo salir de casa? La página que te resuelve todas las dudas."
         />
       </Head>
 
