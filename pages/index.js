62082ba5c85f76ef4c5bba918b9aabb38b86baf5
paulotijero <paulotijero@gmail.com>
2020-03-25 00:11:44 -0500

    FIX: gift centred responsive

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,26 @@ export default function Index() {
         <link rel="icon" href="/question.png" />
       </Head>
 
-      <main className="text-center">
-        <img src={result.image_original_url} alt={result.title} />
+      <main className="flex justify-center h-full w-full">
+        <div className="md:w-6/12 w-11/12 h-4/5 max-w-screen-sm">
+          <img
+            className="object-contain w-full h-full "
+            src={result.image_original_url}
+            alt={result.title}
+          />
+        </div>
       </main>
 
-      <footer className="text-gray-500 py-6">
+      <footer className="text-gray-500 py-4">
         <a
-          href="https://github.com/paulotijero"
+          href="https://paulotijero.dev/"
           target="_blank"
           rel="noopener noreferrer"
+          title="Code with love by Paulo Tijero"
         >
-          Powered by Paulo Tijero
+          <span className="font-bold">{"<>"}</span>
+          <span> with 🖤 by </span>
+          <span className="font-bold">Paulo Tijero</span>
         </a>
       </footer>
 
