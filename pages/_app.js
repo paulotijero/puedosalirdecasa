9cc0838f378ad3c5b51be042fdfd7372335c8720
paulotijero <paulotijero@gmail.com>
2020-07-17 15:17:11 -0500

    ADD: promise load page

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,14 @@ import Head from "next/head"
 import "../assets/styles.css";
 
 export default class CanApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    return { pageProps }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
